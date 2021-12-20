f3aaaacc6c12d47f7dd8793bd7804b458d53e6fe
Deisy Carolina Monroy Gutiérrez <86481741+dcmg03@users.noreply.github.com>
2021-12-19 23:25:38 -0500

    :)

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -8,55 +8,74 @@ import model.Libro;
 import javax.swing.*;
 
 public class Main {
+    static ListaCircular<Libro> lista = new ListaCircular<>();
+    static ArbolLibros<Libro> arbol = new ArbolLibros<>((((Id1, Id2) -> (Id1.getIdLibro().compareTo(Id2.getIdLibro())))));
 
     public static void main(String[] args) throws Exception {
+    menu();
 
-        ListaCircular<Libro> lista = new ListaCircular<>();
-        ArbolLibros<Libro> arbol = new ArbolLibros<>((((Id1, Id2) -> (Id1.getIdLibro().compareTo(Id2.getIdLibro())))));
 
-        int op=0;
+    }
+
+    public static void menu() {
+
+        int op = 0;
         Libro libro = null;
         String idLibro = null;
         String titulo = null;
         String autor = null;
-        String menu="";
-        do{
-            menu="";
-            menu+="****** Menú ******\n";
-            menu+="1.- Agregar libro \n";
-            menu+="2.- Eliminar libro \n";
-            menu+="3.- Buscar libro \n";
-            menu+="4.- Mostrar lista de libros\n";
-            menu+="5.- Salir \n";
-            menu+="Elija una opción";
-            op=Integer.parseInt(JOptionPane.showInputDialog(menu));
+        String menu = "";
+        try {
+            do {
 
-            switch(op){
-                case 1:
-                    idLibro = JOptionPane.showInputDialog("Escriba el id del libro");
-                    titulo = JOptionPane.showInputDialog("Escriba el titulo del libro");
-                    autor = JOptionPane.showInputDialog("Escriba el autor del libro");
-                    lista.agregarALista(libro);
-                    arbol.addNode(libro);
+                menu = "";
+                menu += "****** Menú ******\n";
+                menu += "1.- Agregar libro \n";
+                menu += "2.- Eliminar libro \n";
+                menu += "3.- Buscar libro \n";
+                menu += "4.- Mostrar lista de libros\n";
+                menu += "5.- Salir \n";
+                menu += "Elija una opción";
+                op = Integer.parseInt(JOptionPane.showInputDialog(menu));
 
-                    break;
-                case 2:
-                    idLibro = JOptionPane.showInputDialog("Ingrese el id del libro a eliminar");
-                    lista.delete(libro);
+                switch (op) {
+                    case 1:
+                        idLibro = JOptionPane.showInputDialog("Escriba el id del libro");
+                        titulo = JOptionPane.showInputDialog("Escriba el titulo del libro");
+                        autor = JOptionPane.showInputDialog("Escriba el autor del libro");
+                        lista.agregarALista(libro);
+                        arbol.addNode(libro);
 
-                    break;
-                case 3:
-                    idLibro = JOptionPane.showInputDialog("Ingrese el id del libro a buscar");
-                    JOptionPane.showMessageDialog(null,arbol.findNode(new Libro(idLibro)));
+                        break;
+                    case 2:
+                        idLibro = JOptionPane.showInputDialog("Ingrese el id del libro a eliminar");
+                        lista.delete(libro);
 
-                    break;
-                case 4:
+                        break;
+                    case 3:
+                        idLibro = JOptionPane.showInputDialog("Ingrese el id del libro a buscar");
+                        JOptionPane.showMessageDialog(null, arbol.findNode(new Libro(idLibro)));
 
-                    break;
+                        break;
+                    case 4:
+
+                        break;
+                }
+            } while (op != 5);
+            int resp = JOptionPane.showConfirmDialog(null, "¿Esta segurode abandonar la aplicacion?", "Alerta!", JOptionPane.YES_NO_OPTION);
+            if (resp==0){
+                JOptionPane.showMessageDialog(null, "Gracias por usar la aplicacion \n " + "Cerrando aplicacion...");
+                System.exit(0);
+            }else{
+                menu();
             }
-        }while(op!=5);
-        int resp = JOptionPane.showConfirmDialog(null, "¿Esta segurode abandonar la aplicacion?", "Alerta!", JOptionPane.YES_NO_OPTION);
-        JOptionPane.showMessageDialog(null,"Gracias por usar la aplicacion \n " + "Cerrando aplicacion...");
+
+        }catch (Exception e){
+            menu();
+        }
+
 
     }
+
 }
+
